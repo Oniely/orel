8df04320eff44dac8370c3e4b03b785e3dc1ac92
Oniely <jabagatniel09@gmail.com>
2026-04-18 18:54:07 +0800

    Tanstack Router Setup with Tauri
    - renamed App.css to global.css
    
    - tanstack setup + tanstack dev tools

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 import { Button, Input } from "@heroui/react";
+import { createFileRoute } from "@tanstack/react-router";
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
 
-function App() {
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+function Home() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
@@ -44,5 +48,3 @@ function App() {
     </main>
   );
 }
-
-export default App;
