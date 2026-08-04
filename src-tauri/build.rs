eceadba1674324fda3988e7128dda4d07c41ec80
Oniely <jabagatniel09@gmail.com>
2026-08-04 21:19:32 +0800

    fix: revert window dpi fix (didn't worked lol)

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,3 @@
 fn main() {
-    let windows = tauri_build::WindowsAttributes::new()
-        .app_manifest(include_str!("windows-app-manifest.xml"));
-    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
-
-    tauri_build::try_build(attributes).expect("failed to run Tauri build script");
+    tauri_build::build()
 }
